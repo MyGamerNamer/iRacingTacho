b25689538de66601efaeaff483c36ee430832e8e
krizzly <seankellyhimself@gmail.com>
2023-06-06 17:35:14 -0400

    Added buttons

--- arduino_telemetry/arduino_telemetry.ino
+++ arduino_telemetry/arduino_telemetry.ino
@@ -1,52 +1,65 @@
 #include <LiquidCrystal.h>
-// Initialize the library with the numbers of the interface pins
-// (D7=12, D6=11, D5=10, D4=9, Enable=3, RS=2)
 LiquidCrystal lcd(2, 3, 9, 10, 11, 12);
 
 const int ledPin = 13; // LED connected to digital pin 13
+const int buttonUp = 7; // Button to go to next page
+const int buttonDown = 8; // Button to go to previous page
+
+int currentPage = 0;
+int totalPages = 3; // Total number of pages
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16, 2);  // initialize the lcd for 16 chars 2 lines, turn on backlight
-  lcd.setCursor(0, 0); // move cursor to beginning of first line
-  lcd.print("TACH Initialized.");
-  lcd.setCursor(0, 1); // move cursor to beginning of first line
-  lcd.print("Waiting for data...");
-  pinMode(ledPin, OUTPUT); // sets the digital pin as output
+  lcd.begin(16, 2);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonUp, INPUT_PULLUP); // Enable internal pull-up resistor
+  pinMode(buttonDown, INPUT_PULLUP); // Enable internal pull-up resistor
 }
 
 void loop() {
   if (Serial.available()) {
     String receivedData = Serial.readStringUntil('\n');
-    receivedData.trim();  // remove any leading/trailing whitespace
+    receivedData.trim();
 
-    int colonIndex1 = receivedData.indexOf(':');  // index of the first colon
-    int commaIndex = receivedData.indexOf(',');  // index of the comma
-    int colonIndex2 = receivedData.lastIndexOf(':');  // index of the second colon
+    int colonIndex1 = receivedData.indexOf(':');
+    int commaIndex = receivedData.indexOf(',');
+    int colonIndex2 = receivedData.lastIndexOf(':');
 
     String rpmString = receivedData.substring(colonIndex1 + 1, commaIndex);
     int engine_rpm = rpmString.toInt();
 
     String revLimiterStatus = receivedData.substring(colonIndex2 + 1);
-    revLimiterStatus.trim();  // remove any leading/trailing whitespace
+    revLimiterStatus.trim();
 
     bool is_rev_limiter_engaged = (revLimiterStatus == "True");
 
     lcd.clear();
-    lcd.setCursor(0, 0);  // move cursor to beginning of first line
-    lcd.print("RPM: ");
-    lcd.print(engine_rpm);
-
-    lcd.setCursor(0, 1); // move cursor to beginning of second line
-    if (is_rev_limiter_engaged) {
-      lcd.print("Rev Limiter: ON");
-      digitalWrite(ledPin, HIGH); // turn the LED on
-      delay(15); // wait for half a second
-      digitalWrite(ledPin, LOW); // turn the LED off
-      delay(15); // wait for half a second
-    } else {
-      lcd.print("Rev Limiter: OFF");
-      digitalWrite(ledPin, LOW); // turn the LED off
+    if (currentPage == 0) {
+      lcd.setCursor(0, 0);
+      lcd.print("RPM: ");
+      lcd.print(engine_rpm);
+      lcd.setCursor(0, 1);
+      if (is_rev_limiter_engaged) {
+        lcd.print("Rev Limiter: ON");
+      } else {
+        lcd.print("Rev Limiter: OFF");
+      }
+    } else if (currentPage == 1) {
+      lcd.setCursor(0, 0);
+      lcd.print("Demo Page 2");
+    } else if (currentPage == 2) {
+      lcd.setCursor(0, 0);
+      lcd.print("Demo Page 3");
     }
+  }
+
+  // Check if buttons are pressed
+  if (digitalRead(buttonUp) == LOW) {
+    currentPage = (currentPage + 1) % totalPages;
+    delay(200); // Debounce delay
+  } else if (digitalRead(buttonDown) == LOW) {
+    currentPage = (currentPage - 1 + totalPages) % totalPages;
+    delay(200); // Debounce delay
   }
 }
