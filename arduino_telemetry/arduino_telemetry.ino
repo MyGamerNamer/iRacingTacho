43ccb4205b0d453ca019cc43a4e30b37f4a8b5b1
krizzly <seankellyhimself@gmail.com>
2023-06-07 16:37:53 -0400

    TFT Improvements

--- arduino_telemetry/arduino_telemetry.ino
+++ arduino_telemetry/arduino_telemetry.ino
@@ -9,68 +9,65 @@
 #define TFT_RST  9  // Set TFT_RST to digital pin 9
 #define TFT_DC   8  // Set TFT_DC to digital pin 8
 
-#define MAX_BUFFER 64 // maximum size of the expected message
+#define TFT_WIDTH  128
+#define TFT_HEIGHT 160
+#define TFT_CENTER_X TFT_WIDTH / 2
+#define TFT_CENTER_Y (TFT_HEIGHT / 2) - 10 // Move tachometer up by 20 pixels
+#define TFT_MIN_X 10
+#define TFT_MIN_Y 10
+#define TFT_MAX_X TFT_WIDTH - 10
+#define TFT_MAX_Y TFT_HEIGHT - 10
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST);
+#define REDLINE_RPM 6000
+#define MAX_RPM 8000
 
-int prev_engine_rpm = -1;
-String prev_rev_limiter_status = "";
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
   Serial.begin(9600);
-  tft.initR(INITR_GREENTAB); // Initialize TFT display
-  tft.fillScreen(ST7735_BLACK); // Clear screen
+  tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(ST7735_BLACK);
+  tft.drawCircle(TFT_CENTER_X, TFT_CENTER_Y, 60, ST7735_WHITE); // Draw tachometer circle
+  tft.setTextWrap(false); // Disable text wrapping
 
-  // Initialize the text on the screen
-  tft.setCursor(40, 20); // Move cursor higher
-  tft.setTextColor(ST7735_WHITE);
-  tft.setTextSize(3);
-  tft.println("RPM: ");
+  // Draw tick marks and labels
+  for (int rpm = 0; rpm <= MAX_RPM; rpm += 1000) {
+    float angle = map(rpm, MAX_RPM, 0, 135, -135) - 90; // Map RPM to angle
+    float radian_angle = angle * PI / 180.0;
+
+    int start_x = TFT_CENTER_X + 60 * cos(radian_angle);
+    int start_y = TFT_CENTER_Y + 60 * sin(radian_angle);
+
+    int end_x = TFT_CENTER_X + 55 * cos(radian_angle);
+    int end_y = TFT_CENTER_Y + 55 * sin(radian_angle);
+
+    tft.drawLine(start_x, start_y, end_x, end_y, rpm > REDLINE_RPM ? ST7735_RED : ST7735_WHITE);
+
+    int text_x = TFT_CENTER_X + 48 * cos(radian_angle);
+    int text_y = TFT_CENTER_Y + 48 * sin(radian_angle);
+
+    // Draw number for tick mark
+    tft.setCursor(text_x, text_y);
+    tft.setTextColor(ST7735_WHITE);
+    tft.setTextSize(1);
+    tft.print(rpm / 1000);
+  }
 }
 
 void loop() {
   while (Serial.available()) {
-    char receivedData[MAX_BUFFER];
-    int size = Serial.readBytesUntil('\n', receivedData, sizeof(receivedData) - 1);
+    int engine_rpm = Serial.parseInt();
 
-    if (size > 0) {
-      receivedData[size] = '\0'; // null terminate the C string
+    // Calculate needle position
+    float angle = map(engine_rpm, MAX_RPM, 0, 135, -135) - 90; // Map RPM to angle
+    float radian_angle = angle * PI / 180.0;
+    int needle_length = 50;
+    int needle_x = TFT_CENTER_X + needle_length * cos(radian_angle);
+    int needle_y = TFT_CENTER_Y + needle_length * sin(radian_angle);
 
-      String strReceivedData = String(receivedData);
-      strReceivedData.trim();
-
-      int colonIndex1 = strReceivedData.indexOf(':');
-      int commaIndex = strReceivedData.indexOf(',');
-      int colonIndex2 = strReceivedData.lastIndexOf(':');
-
-      String rpmString = strReceivedData.substring(colonIndex1 + 1, commaIndex);
-      int engine_rpm = rpmString.toInt();
-
-      if (engine_rpm != prev_engine_rpm) {
-        tft.fillRect(40, 50, 100, 20, ST7735_BLACK); // clear RPM text
-        tft.setCursor(40, 50); // Move cursor higher
-        tft.setTextColor(ST7735_WHITE);
-        tft.setTextSize(3);
-        tft.println(engine_rpm);
-        prev_engine_rpm = engine_rpm;
-      }
-
-      String revLimiterStatus = strReceivedData.substring(colonIndex2 + 1);
-      revLimiterStatus.trim();
-
-      if (revLimiterStatus != prev_rev_limiter_status) {
-        tft.fillRect(0, 90, 128, 68, ST7735_BLACK); // clear rev limiter text
-        tft.setCursor(10, 100); // Adjust position for "REV LIMITER"
-        tft.setTextSize(1.8);
-        tft.println("REV LIMITER");
-        if (revLimiterStatus == "True") {
-          tft.fillRect(0, 90, 128, 68, ST7735_RED); // Fill bottom half of the screen with red
-          tft.setCursor(10, 100); // Adjust position for "REV LIMITER"
-          tft.setTextColor(ST7735_WHITE);
-          tft.println("REV LIMITER");
-        }
-        prev_rev_limiter_status = revLimiterStatus;
-      }
-    }
+    // Draw needle
+    tft.drawLine(TFT_CENTER_X, TFT_CENTER_Y, needle_x, needle_y, ST7735_RED);
+    delay(100); // Wait to see the needle
+    tft.drawLine(TFT_CENTER_X, TFT_CENTER_Y, needle_x, needle_y, ST7735_BLACK); // Erase the needle
   }
 }
