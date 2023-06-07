c7ede644337f3e84178e08e2524342f2ef2c26e8
krizzly <seankellyhimself@gmail.com>
2023-06-07 16:05:01 -0400

    TFT Improvements

--- arduino_telemetry/arduino_telemetry.ino
+++ arduino_telemetry/arduino_telemetry.ino
@@ -9,50 +9,68 @@
 #define TFT_RST  9  // Set TFT_RST to digital pin 9
 #define TFT_DC   8  // Set TFT_DC to digital pin 8
 
-#define LED_PIN  7  // LED on pin 7
+#define MAX_BUFFER 64 // maximum size of the expected message
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST);
 
+int prev_engine_rpm = -1;
+String prev_rev_limiter_status = "";
+
 void setup() {
   Serial.begin(9600);
-  tft.initR(INITR_BLACKTAB); // Initialize TFT display
+  tft.initR(INITR_GREENTAB); // Initialize TFT display
   tft.fillScreen(ST7735_BLACK); // Clear screen
 
-  pinMode(LED_PIN, OUTPUT);
+  // Initialize the text on the screen
+  tft.setCursor(40, 20); // Move cursor higher
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(3);
+  tft.println("RPM: ");
 }
 
 void loop() {
-  if (Serial.available()) {
-    String receivedData = Serial.readStringUntil('\n');
-    receivedData.trim();
+  while (Serial.available()) {
+    char receivedData[MAX_BUFFER];
+    int size = Serial.readBytesUntil('\n', receivedData, sizeof(receivedData) - 1);
 
-    int colonIndex1 = receivedData.indexOf(':');
-    int commaIndex = receivedData.indexOf(',');
-    int colonIndex2 = receivedData.lastIndexOf(':');
+    if (size > 0) {
+      receivedData[size] = '\0'; // null terminate the C string
 
-    String rpmString = receivedData.substring(colonIndex1 + 1, commaIndex);
-    int engine_rpm = rpmString.toInt();
+      String strReceivedData = String(receivedData);
+      strReceivedData.trim();
 
-    String revLimiterStatus = receivedData.substring(colonIndex2 + 1);
-    revLimiterStatus.trim();
+      int colonIndex1 = strReceivedData.indexOf(':');
+      int commaIndex = strReceivedData.indexOf(',');
+      int colonIndex2 = strReceivedData.lastIndexOf(':');
 
-    bool is_rev_limiter_engaged = (revLimiterStatus == "True");
+      String rpmString = strReceivedData.substring(colonIndex1 + 1, commaIndex);
+      int engine_rpm = rpmString.toInt();
 
-    tft.fillScreen(ST7735_BLACK); // Clear screen
+      if (engine_rpm != prev_engine_rpm) {
+        tft.fillRect(40, 50, 100, 20, ST7735_BLACK); // clear RPM text
+        tft.setCursor(40, 50); // Move cursor higher
+        tft.setTextColor(ST7735_WHITE);
+        tft.setTextSize(3);
+        tft.println(engine_rpm);
+        prev_engine_rpm = engine_rpm;
+      }
 
-    tft.setCursor(20, 50); // Adjust these values to center text
-    tft.setTextColor(ST7735_WHITE);
-    tft.setTextSize(2);  // Increase text size
-    tft.print("RPM: ");
-    tft.println(engine_rpm);
+      String revLimiterStatus = strReceivedData.substring(colonIndex2 + 1);
+      revLimiterStatus.trim();
 
-    tft.setCursor(20, 70); // Adjust these values to center text
-    if (is_rev_limiter_engaged) {
-      tft.println("Rev Limiter: ON");
-      digitalWrite(LED_PIN, HIGH);
-    } else {
-      tft.println("Rev Limiter: OFF");
-      digitalWrite(LED_PIN, LOW);
+      if (revLimiterStatus != prev_rev_limiter_status) {
+        tft.fillRect(0, 90, 128, 68, ST7735_BLACK); // clear rev limiter text
+        tft.setCursor(10, 100); // Adjust position for "REV LIMITER"
+        tft.setTextSize(1.8);
+        tft.println("REV LIMITER");
+        if (revLimiterStatus == "True") {
+          tft.fillRect(0, 90, 128, 68, ST7735_RED); // Fill bottom half of the screen with red
+          tft.setCursor(10, 100); // Adjust position for "REV LIMITER"
+          tft.setTextColor(ST7735_WHITE);
+          tft.println("REV LIMITER");
+        }
+        prev_rev_limiter_status = revLimiterStatus;
+      }
     }
   }
 }
