bbf7516aa2626c7a3d49636dc6d4f00a61935494
krizzly <seankellyhimself@gmail.com>
2023-06-08 17:51:30 -0400

    requirements.txt

--- testing.py
+++ testing.py
@@ -4,40 +4,33 @@ import sys
 # Initialize Pygame
 pygame.init()
 
-# Set the size of the window
-size = (700, 500)
-screen = pygame.display.set_mode(size)
+# Set the dimensions of the window
+window_size = (800, 600)
+screen = pygame.display.set_mode(window_size)
 
-# Set the title of the window
-pygame.display.set_caption("My First Pygame Program")
-
-# Loop until the user clicks the close button
+# Main loop
 done = False
-
-# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+try:
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
-    # --- Drawing code should go here
-    # First, clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command
-    screen.fill((255, 255, 255))
+        # Fill the screen with white
+        screen.fill((255, 255, 255))
 
-    # Draw a red circle
-    pygame.draw.circle(screen, (255, 0, 0), (350, 250), 50)
+        # Draw a red square
+        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(30, 30, 60, 60))
 
-    # --- Go ahead and update the screen with what we've drawn
-    pygame.display.flip()
+        # Update the full display surface
+        pygame.display.flip()
 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-
-# Close the window and quit
-pygame.quit()
-sys.exit()
+        # Limit to 60 frames per second
+        clock.tick(60)
+except KeyboardInterrupt:
+    done = True
+finally:
+    pygame.quit()
+    sys.exit()
